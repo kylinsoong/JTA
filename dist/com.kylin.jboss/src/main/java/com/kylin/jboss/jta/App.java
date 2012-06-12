42a762a408cbb8f0d72104f1333a4a87898eb7b8
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-06-12 18:08:34 +0800

    add jboss jta test projects

---
+++ dist/com.kylin.jboss/src/main/java/com/kylin/jboss/jta/App.java
@@ -0,0 +1,13 @@
+package com.kylin.jboss.jta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
